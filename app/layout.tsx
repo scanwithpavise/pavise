da9fdc56cd299b4fbaae6325b43e4b8370bbedc8
Shin <62398165+nakamurashin01@users.noreply.github.com>
2025-03-03 01:13:13 +0800

    Update SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,22 @@ const inter = Inter({ subsets: ["latin"] });
 import { headers } from "next/headers";
 export const metadata: Metadata = {
   title: "PAVISE",
-  description: "Powered by Reown",
+  description: "Pavise first AI-powered Health Diagnostic",
+  openGraph: {
+    title: "PAVISE - AI Health Diagnostic",
+    description : "Discover the power of AI-driven health diagnostics with Pavise.",
+    url: "https://scanwithpavise.com",
+    siteName: "scanwithpavise",
+    images : [
+      {
+        url : "https://pavise-ohayvuk6e-imrybrndos-projects.vercel.app/Images/pavise.jpeg",
+        width : 1200,
+        height : 630,
+        alt : "PAVISE AI Health Diagnostic"
+      }
+    ],
+    type: "website",
+  },
 };
 export default async function RootLayout({
   children,
